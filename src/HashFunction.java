d89b2ac4306819fbbf4e35587eb5a9962d9ec76d
sandervw <sanderv@iastate.edu>
2017-02-19 17:26:52 -0600

    commit of class setup and work for Tuple class.

---
+++ src/HashFunction.java
@@ -0,0 +1,5 @@
+
+/* This class represents a random hash function that can be used in a hash table */
+public class HashFunction {
+
+}
---
+++ src/Tuple.java
@@ -0,0 +1,35 @@
+
+/* This class represents tuples in the form of a key value pair */
+public class Tuple {
+	
+	private int key;
+	private float value;
+	
+	/* Constructor */
+	public Tuple(int keyP, float valueP){
+		
+		this.key = keyP;
+		this.value = valueP;
+		
+	}
+	
+	public int getKey(){
+		
+		return key;
+		
+	}
+	
+	public float getValue(){
+		
+		return value;
+		
+	}
+	
+	public boolean equals(Tuple t){
+		
+		if(t.getKey() == this.key && t.getValue() == this.value) return true;
+		else return false;
+		
+	}
+
+}
