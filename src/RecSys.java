1bee267c989550caedf9d6a79291e64910898e3f
zvlovisa <zvlovisa@iastate.edu>
2017-02-25 13:39:32 -0600

    Added text file and continued work on recsys

--- src/RecSys.java
+++ src/RecSys.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class RecSys {
 	
@@ -9,20 +13,50 @@ public class RecSys {
 	 * 5
 	 */
 	public RecSys(String mrMatrix){
-		int p = 0;
-		h = new HashTable(p);
+		
+		int tempSize = 0;
+		int u;
+		int m;
+		//add the lines to an array
+		Scanner s = null;
+		try {
+			s = new Scanner(new File(mrMatrix));
+			String[] line = s.nextLine().split(" ", 2);
+			
+			u = Integer.parseInt(line[0]);
+			m = Integer.parseInt(line[1]);
+			float[] users = new float[u];
+			int[] movieRatings = new int[m];
+			for(int i = 0; i < u; i++){
+				line = s.nextLine().split(" ", m+1);
+				users[i] = Float.parseFloat(line[0]);
+				for(int j = 1; j < line.length; j++){
+					movieRatings[j-1] = Integer.parseInt(line[j]);
+				}
+			}
+			s.close();
+			//initialize p
+			h= new HashTable(tempSize);
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	/**
 	 * If the user u has rated movie m, then it returns that rating; otherwise
 	 * it will predict the rating based on the approach described above, and returns the predicted rating.
 	 * The type of this method must be float.
-	 * @param u
-	 * @param m
+	 * @param u User 
+	 * @param m Movie (key) 
 	 * 
-	 * @return
+	 * @return The predicted rating of the movie for this user.
 	 */
-	public double ratingOf(int u, int m){
+	public float ratingOf(int u, int m){
+		ArrayList<Tuple> ratings = h.search(u);
+		NearestPoint np = 
+		
 		return -1;
 	}
 }
